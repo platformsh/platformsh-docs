ad8675918f8f1917552f96dc1941c1c1865d956b
Ori Pekelman <ori@pekelman.com>
2015-05-18 10:54:09 +0200

    Use realpath
    ... to be sure we are loading what we wanted to

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $path = parse_url($_SERVER['REQUEST_URI']);
 $path = ltrim(rtrim($path["path"], '/'), '.'); //trim trailing slash and prefix dots
-$index = getcwd() .$path ."/index.html";
-if (file_exists($index)){
+$index = realpath(getcwd() .$path ."/index.html");
+if (substr($index, 0, strlen(realpath(getcwd()))) === realpath(getcwd()) && file_exists($index)){
   readfile($index);
 } else { 
   http_response_code(404);
