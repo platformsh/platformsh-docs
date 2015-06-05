d55abe3a67b6c62431b1055b281601e25adc0386
dudenhofer <dudenhofer@gmail.com>
2015-06-05 08:45:12 -0400

    Using prop

--- node_modules/gitbook-theme-platform/book/platform.js
+++ node_modules/gitbook-theme-platform/book/platform.js
@@ -1,5 +1,5 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("page.change", function() {
-        $('.book-search input[type="text"]').attr('placeholder', 'Search');
+        $('.book-search input[type="text"]').prop('placeholder', 'Search');
     });
 });
