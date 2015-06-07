06c751e704dea271351b439352207b48e33e8d34
Ori Pekelman <ori@pekelman.com>
2015-06-07 21:02:16 +0200

    a and div tags were in incorrect order

--- node_modules/gitbook-theme-platform/index.js
+++ node_modules/gitbook-theme-platform/index.js
@@ -9,7 +9,7 @@ module.exports = {
         ],
         html: {
           "head:end": "<link rel='stylesheet' type='text/css' href='//cloud.typography.com/7640312/659566/css/fonts.css'>",
-          "body:start": "<div id='platform-bar'><a class='branding' href='/'><span class='logo'></span><span class='site-name'>Docs</span></div></a>"
+          "body:start": "<div id='platform-bar'><a class='branding' href='/'><span class='logo'></span><span class='site-name'>Docs</span></a></div>"
         }
     },
     hooks: {
