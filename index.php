1ef4f7d024655f1247ce750b540b9c32efe3f37b
Damien Tournoud <damien@platform.sh>
2015-06-10 12:37:48 +0200

    Add explicit caching header.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $path = parse_url($_SERVER['REQUEST_URI']);
 $path = ltrim(rtrim($path["path"], '/'), '.'); //trim trailing slash and prefix dots
 $index = realpath(getcwd() .$path ."/index.html");
 if (substr($index, 0, strlen(realpath(getcwd()))) === realpath(getcwd()) && file_exists($index)){
+  header("Cache-Control: public, max-age=300");
   readfile($index);
 } else { 
   http_response_code(404);
