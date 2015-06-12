0c2e622c494bc7f2fda12e6aba649ec398e10d31
dudenhofer <dudenhofer@gmail.com>
2015-06-12 14:21:36 -0400

    Replacing glossary links with tooltips

--- node_modules/gitbook-theme-platform/book/platform.js
+++ node_modules/gitbook-theme-platform/book/platform.js
@@ -1,5 +1,14 @@
 require(["gitbook"], function(gitbook) {
-    gitbook.events.bind("page.change", function() {
-        $('.book-search input[type="text"]').prop('placeholder', 'Search');
+  gitbook.events.bind("page.change", function() {
+
+    $('.book-search input[type="text"]').prop('placeholder', 'Search');
+
+    // Replacing glossary term elements.
+    $('.glossary-term').each(function(){
+      var message = $(this).prop('title');
+      var term = $(this).text();
+      $(this).replaceWith('<span class="glossary-term">' + term + '<span class="tooltip-wrapper"><span class="tooltip">' + message + '</span></span></span>');
     });
+
+  });
 });
