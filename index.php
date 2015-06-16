8b0132a848f383dc00d8caeea868076561fabf08
Augustin Delaporte <augustin@commerceguys.com>
2015-06-16 15:32:19 +0200

    I don't need you anymore.

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ $url_mapping = array(
 $path = parse_url($_SERVER['REQUEST_URI']);
 $path = ltrim(rtrim($path["path"], '/'), '.'); //trim trailing slash and prefix dots
 $index = realpath(getcwd() .$path ."/index.html");
-var_dump($path);
 if (substr($index, 0, strlen(realpath(getcwd()))) === realpath(getcwd()) && file_exists($index)){
   header("Cache-Control: public, max-age=300");
   readfile($index);
