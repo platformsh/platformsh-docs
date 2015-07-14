94e8f8a47b5f408958897c97f1f53bac23b43355
dudenhofer <dudenhofer@gmail.com>
2015-07-14 16:11:42 +0200

    Updating book titles, adding hardcoded dropdown menu, updating links and fixing some homepage styles

--- node_modules/gitbook-theme-platform/index.js
+++ node_modules/gitbook-theme-platform/index.js
@@ -10,16 +10,15 @@ module.exports = {
         html: {
           "head:end": "<link rel='stylesheet' type='text/css' href='//cloud.typography.com/7640312/659566/css/fonts.css'>",
           "body:start": function() {
-            return "<div id='platform-bar'><a class='branding' href='/'><span class='logo'></span><span class='site-name'>Docs</span></a><div class='navlink'><span>/</span>"+this.options.title+"</div></div>"
+            return "<div id='platform-bar'><a class='branding' href='/'><span class='logo'></span><span class='site-name'>Docs</span></a><div id='navlink'><span class='divider'>/</span><div class='dropdown'><ul><li>"+this.options.title+"<ul><li><a href='/overview/'>Overview</a></li><li><a href='/drupal/'>Drupal</a></li><li><a href='/drupal_migrate/'>Drupal Migrate</a></li><li><a href='/symfony/'>Symfony</a></li><li><a href='/reference/'>Reference</a></li></ul></li></ul></div></div></div>"
           }
         }
     },
     hooks: {
         init: function() {
-            console.log("init!");
+
         },
         finish: function() {
-            console.log("finish!");
         }
     }
 };
