508092c1b89c355a109581072661457372f15185
Augustin Delaporte <augustin@commerceguys.com>
2015-07-15 10:32:28 +0200

    Fix SSH redirect link.

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 # Mapping of old URLs as key, and new URLs as values.
 $url_mapping = array(
   '/web-ui/configure-environment' => '/overview/web-ui/configure-environment.html',
-  '/web-ui/configure-project' => '/overview/web-ui/configure-project.html'
+  '/web-ui/configure-project' => '/overview/web-ui/configure-project.html',
+  '/overview/technical-requirements' => 'using/use-SSH.html'
 );
 # Load HTML files until Platform.sh supports directory indexes.
 $path = parse_url($_SERVER['REQUEST_URI']);
