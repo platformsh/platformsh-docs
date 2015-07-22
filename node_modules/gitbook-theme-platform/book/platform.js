0f86bff5fa4f38677ff9434acc7cbeabde790ff3
GuGuss <augustin@commerceguys.com>
2015-07-22 16:21:35 +0200

    Fix Search box disapearance @OriPekelman++

--- node_modules/gitbook-theme-platform/book/platform.js
+++ node_modules/gitbook-theme-platform/book/platform.js
@@ -1,5 +1,7 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("page.change", function() {
         $('.book-search input[type="text"]').prop('placeholder', 'Search');
+        $('.book-search').css('top', '73px');
+        $('.book-search').css('display', 'block');
     });
 });
