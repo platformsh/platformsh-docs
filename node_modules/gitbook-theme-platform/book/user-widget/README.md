f2f8767d9354d0fac2c1eef1c6c148c0aa1c187b
dudenhofer <dudenhofer@gmail.com>
2015-09-16 13:39:19 -0400

    Updating user widget to latest code

--- node_modules/gitbook-theme-platform/book/user-widget/README.md
+++ node_modules/gitbook-theme-platform/book/user-widget/README.md
@@ -6,9 +6,10 @@ Creates the user widget for Platform.sh websites.
 Installation
 ------------
 
-This plugin requires Angularjs. The following 3 scripts are required in the order, but with the proper URLs.
+This plugin requires Angularjs. The following 4 scripts are required in order, but with the proper URLs.
 
-```
+```html
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="[root]/user-widget/user-widget.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.18/angular.min.js"></script>
 <script src="[root]/user-widget/app.js"></script>
@@ -16,6 +17,6 @@ This plugin requires Angularjs. The following 3 scripts are required in the orde
 
 Then add the css to get the styling.
 
-```
+```html
 <link rel="stylesheet" type="text/css" href="[root]/user-widget/user-widget.css" />
 ```
--- node_modules/gitbook-theme-platform/book/user-widget/app.js
+++ node_modules/gitbook-theme-platform/book/user-widget/app.js
@@ -5,87 +5,39 @@
  */
 
 angular.module('userWidget',[])
-  .controller('navBar', function($scope, $http){
-    $scope.accountsPath = 'https://accounts.platform.sh';
+  .controller('navBar', function($scope, $http) {
+    $scope.accountsPath = 'https://accounts.internal.platform.sh';
+    if (typeof(accountsPath) !== "undefined") {
+      $scope.accountsPath = accountsPath;
+    }
     $scope.defaultUserPic = $scope.accountsPath + '/profiles/blimp_profile/themes/bootstrap_accounts/images/user-default.png';
 
-    $scope.platform = [];
+    $scope.platform = {};
 
-    // This is the hardcoded json array provided by Christian.
-    $scope.platform = {
-      "projects": [
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/klsl6lh2ord3c",
-          "name": "Awesome Website",
-          "plan": "Development",
-          "license_id": "5755"
-        },
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/6rw4zftgkblqc",
-          "name": "TMC 2.x Test",
-          "plan": "Development",
-          "license_id": "5588"
-        },
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/klsl6lh2ord3c",
-          "name": "Awesome Website",
-          "plan": "Development",
-          "license_id": "5755"
-        },
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/6rw4zftgkblqc",
-          "name": "TMC 2.x Test",
-          "plan": "Development",
-          "license_id": "5588"
-        },
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/klsl6lh2ord3c",
-          "name": "Awesome Website",
-          "plan": "Development",
-          "license_id": "5755"
-        },
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/6rw4zftgkblqc",
-          "name": "TMC 2.x Test",
-          "plan": "Development",
-          "license_id": "5588"
-        },
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/klsl6lh2ord3c",
-          "name": "Awesome Website",
-          "plan": "Development",
-          "license_id": "5755"
-        },
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/6rw4zftgkblqc",
-          "name": "TMC 2.x Test",
-          "plan": "Development",
-          "license_id": "5588"
-        },
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/klsl6lh2ord3c",
-          "name": "Awesome Website",
-          "plan": "Development",
-          "license_id": "5755"
-        },
-        {
-          "uri": "https:\/\/us.platform.sh\/#\/projects\/6rw4zftgkblqc",
-          "name": "TMC 2.x Test",
-          "plan": "Development",
-          "license_id": "5588"
-        }
-      ],
-      "user": {
-        "name": "Admin Marketplace",
-        "uid": "1",
-        "uuid": "d7c034f8-d9ba-43a2-898a-1dc96f84913d",
-        "mail": "user_1@example.com",
-        "picture": "http:\/\/marketplace.dev\/sites\/default\/files\/pictures\/picture-208-1436972167.jpg",
-        "links": [
-          "https:\/\/marketplace.commerceguys.com\/user",
-          "https:\/\/marketplace.commerceguys.com\/user\/logout"
-        ]
+    $scope.loading = true;
+    $http.get($scope.accountsPath + "/api/platform/self", {
+        withCredentials: true
+      })
+      .success(function (data) {
+        $scope.platform.user = data;
+        $scope.platform.projects = data.projects;
+      })
+      .finally(function(){
+        $scope.loading = false;
+      });
+
+    var indexedPlatforms = [];
+    $scope.platformstoFilter = function(){
+      indexedPlatforms = [];
+      return $scope.platform.projects;
+    };
+
+    $scope.filterProjects = function(project) {
+      var projectIsNew = indexedPlatforms.indexOf(project.region_label) == -1;
+      if (projectIsNew) {
+        indexedPlatforms.push(project.region_label);
       }
-    }
+      return projectIsNew;
+    };
 
   });
