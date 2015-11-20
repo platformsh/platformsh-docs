6feaf8b7b442835320c7ed701ed285904dd44db4
Ori Pekelman <ori+github@pekelman.com>
2015-11-20 17:13:12 +0100

    update dependecies

--- node_modules/gitbook-plugin-anchors/index.js
+++ node_modules/gitbook-plugin-anchors/index.js
@@ -1,36 +1,33 @@
-
 var cheerio = require('cheerio');
-var _ = require('underscore');
+var _ = require('lodash');
 
 // insert anchor link into section
-var insertAnchors = function(section) {
-    
-    var $ = cheerio.load(section.content);
-    
+function insertAnchors(content) {
+    var $ = cheerio.load(content);
     $(':header').each(function(i, elem) {
-	var header = $(elem);
-	var id = header.attr('id');
-	header.prepend('<a name="' + id + '" class="plugin-anchor" ' 
-		       + 'href="#' + id + '">' 
-		       + '<span class="fa fa-link"></span>'
-		       + '</a>');
+        var header = $(elem);
+        var id = header.attr('id');
+        header.prepend('<a name="' + id + '" class="plugin-anchor" '
+                   + 'href="#' + id + '">'
+                   + '<span class="fa fa-link"></span>'
+                   + '</a>');
     });
-    section.content = $.html();
-};
+    return $.html();
+}
 
 module.exports = {
     book: {
-        assets: ".",
+        assets: "./assets",
         css: [ "plugin.css" ]
     },
     hooks: {
-        "page": function(page) { // before html generation
-	    
-	    sections = _.select(page.sections, function(section) {
-		return section.type == 'normal';
-	    }); // pluck all normal sections -- as opposed to exercises?
+        "page": function(page) {
+            page.sections = _.map(page.sections, function(section) {
+                if (section.type != 'normal') return section;
 
-	    _.forEach(sections, insertAnchors);
+                section.content = insertAnchors(section.content);
+                return section;
+            });
 
             return page;
         }
--- node_modules/gitbook-plugin-edit-link/index.js
+++ node_modules/gitbook-plugin-edit-link/index.js
@@ -3,50 +3,6 @@ var path = require('path');
 module.exports = {
     book: {
         assets: "./book",
-        js: ["plugin.js"],
-        css: ["plugin.css"]
-    },
-    hooks: {
-        // After html generation
-        "page": function(page) {
-            if(this.config.options.generator !== 'website') {
-                return page;
-            }
-
-            var config = this.options.pluginsConfig["edit-link"] || {};
-
-            if (!config.base) {
-                throw "ERROR: 'base' value required to generate 'Edit This Page' link. \nFor help, please refer to - https://github.com/rtCamp/gitbook-plugin-edit-link/blob/master/README.md#usage";
-            }
-
-            var label = (config.label && config.label[this.context.config.language]) || config.label || "Edit This Page";
-
-            // add  slash at the end if not present
-            var base = config.base;
-            if(base.slice(-1) != "/") {
-                base = base + "/";
-            }
-
-            // relative path to the page
-            var newPath = path.relative(this.root, page.rawPath);
-
-            // language, if configured
-            var lang = "";
-            if(this.context.config.language) {
-                lang = this.context.config.language + "/";
-            }
-
-            rtEditLink = '<a id="edit-link" href="' + base + lang + newPath + '" class="btn fa fa-edit pull-left">&nbsp;&nbsp;' + label + '</a>';
-
-            page.sections
-                .filter(function(section) {
-                    return section.type == 'normal';
-                })
-                .forEach(function(section) {
-                    section.content = rtEditLink + section.content;
-                });
-
-            return page;
-        }
+        js: ["plugin.js"]
     }
 };
