c1225f2fef6d045e979c3d313a81c1b3253f1b63
dudenhofer <dudenhofer@gmail.com>
2017-01-12 10:33:57 -0500

    update submodule and trigger datalayer push on page change

--- src/scripts/user-widget.js
+++ src/scripts/user-widget.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+  require(["gitbook"], function(gitbook) {
+    gitbook.events.bind("page.change", function(e) {
+      if (gitbook.state.config.pluginsConfig.gtm.virtualPageViews) {
+        var thisPath = window.location.pathname;
+        var str = thisPath.split("/");
+        var category = str[1].charAt(0).toUpperCase() + str[1].slice(1) || 'Home';
+        var dataLayer = window.dataLayer || [];
+        dataLayer.push({
+          'PageTitle': gitbook.state.page.title,
+          'PageCategory': category
+        });
+      }
+    });
+  });
+
   // Hard-code the values for the Documentation setup in particular.
   var sourcePath = '\'/scripts/user-widget/';
   var containerWrapper = '#platform-bar';
