f0997165c6e20245865ac91a9375decd0db78d45
chadcarlson <chad.carlson@platform.sh>
2019-07-18 12:43:33 -0400

    Revert buttons to refresh issue.

--- src/scripts/buttons.js
+++ src/scripts/buttons.js
@@ -2,25 +2,37 @@
  * Make any "next" and "prev" links point to the right URL dynamically.
  */
 
-require(["gitbook"], function(gitbook) {
+document.addEventListener('DOMContentLoaded', () => {
+ let nextLinkTarget = document.querySelector("link[rel=next]").getAttribute("href")
+ document.querySelectorAll('a.button-link.next').forEach((elm) => {
+   elm.setAttribute('href', nextLinkTarget);
+ });
 
-  document.addEventListener('DOMContentLoaded', () => {
-    let nextLinkTarget = document.querySelector("link[rel=next]").getAttribute("href")
-    document.querySelectorAll('a.button-link.next').forEach((elm) => {
-      elm.setAttribute('href', nextLinkTarget);
-    });
-
-    let prevLinkTarget = document.querySelector("link[rel=prev]").getAttribute("href");
-    document.querySelectorAll('a.button-link.prev').forEach((elm) => {
-      elm.setAttribute('href', prevLinkTarget);
-    });
-  });
-
-  gitbook.events.bind("page.change", function() {
-       track("page.change");
-  });
-
+ let prevLinkTarget = document.querySelector("link[rel=prev]").getAttribute("href");
+ document.querySelectorAll('a.button-link.prev').forEach((elm) => {
+   elm.setAttribute('href', prevLinkTarget);
+ });
 });
+//
+// require(["gitbook"], function(gitbook) {
+//
+//   document.addEventListener('DOMContentLoaded', () => {
+//     let nextLinkTarget = document.querySelector("link[rel=next]").getAttribute("href")
+//     document.querySelectorAll('a.button-link.next').forEach((elm) => {
+//       elm.setAttribute('href', nextLinkTarget);
+//     });
+//
+//     let prevLinkTarget = document.querySelector("link[rel=prev]").getAttribute("href");
+//     document.querySelectorAll('a.button-link.prev').forEach((elm) => {
+//       elm.setAttribute('href', prevLinkTarget);
+//     });
+//   });
+//
+//   gitbook.events.bind("page.change", function() {
+//        track("page.change");
+//   });
+//
+// });
 
 // function ready(fn) {
 //   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
