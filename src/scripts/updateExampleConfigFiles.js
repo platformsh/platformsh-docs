36bdb0451a2d1688529edd199eb23da85a3e3b36
chadcarlson <chad.carlson@platform.sh>
2019-08-30 17:09:11 -0400

    Adds pshregistry-parser to generate config files on build.

---
+++ src/scripts/updateExampleConfigFiles.js
@@ -0,0 +1,7 @@
+const psh = require("pshregistry-parser");
+
+
+let registrySource = "_book/registry/images/registry.json";
+let registry = new psh.RegistryParser(registrySource);
+
+registry.write();
