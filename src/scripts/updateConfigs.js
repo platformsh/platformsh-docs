efd3c9cfda445a96b1ffcc2133ca2dbacbc70495
chadcarlson <chad.carlson@platform.sh>
2019-08-30 16:33:07 -0400

    Use pshregistry-parser to write files during build.

--- src/scripts/updateConfigs.js
+++
@@ -1,7 +0,0 @@
-const psh = require("pshregistry-parser");
-
-
-let registrySource = "src/registry/images/registry.json";
-let saveDirectory = "src/registry/images/examples/";
-let registry = new psh.RegistryParser(registrySource, saveDirectory);
-registry.write();
