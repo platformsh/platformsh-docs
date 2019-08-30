9f8b411282fda2dc6989f79e766c5111f8904c84
chadcarlson <chad.carlson@platform.sh>
2019-08-30 16:43:42 -0400

    Move registry source to _book.

--- src/scripts/updateExampleConfigFiles.js
+++ src/scripts/updateExampleConfigFiles.js
@@ -1,6 +1,6 @@
 const psh = require("pshregistry-parser");
 
 
-let registrySource = "src/registry/images/registry.json";
+let registrySource = "_book/registry/images/registry.json";
 let registry = new psh.RegistryParser(registrySource);
 registry.write();
