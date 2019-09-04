bf2687c7ce4bd779958cbaa888c2c213b43a20b0
chadcarlson <chad.carlson@platform.sh>
2019-09-04 11:40:03 -0400

    Newest version, adds Go 1.13. Deletes yaml.

--- src/scripts/updateExampleConfigFiles.js
+++ src/scripts/updateExampleConfigFiles.js
@@ -1,7 +1,7 @@
 const psh = require("pshregistry-parser");
 
 
-let registrySource = "_book/registry/images/registry.json";
-let registry = new psh.RegistryParser(registrySource);
+const registrySource = "_book/registry/images/registry.json";
+const registry = new psh.RegistryParser(registrySource);
 
 registry.write();
