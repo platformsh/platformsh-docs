1756ee553be4fdade3d31ecdc02653a4fa12f746
chadcarlson <chad.carlson@platform.sh>
2020-11-02 09:22:37 -0500

    Try the get-poetry method.

--- search/build.sh
+++ search/build.sh
@@ -4,8 +4,9 @@
 curl -L https://install.meilisearch.com | sh
 
 # Install poetry
-pip install --upgrade pip
-pip install poetry
+# pip install --upgrade pip
+# pip install poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 
 # Install dependencies for scrapy
 poetry install
