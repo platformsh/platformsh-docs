549134a213915f1e097c4df525182cf13e53bf67
chadcarlson <chad.carlson@platform.sh>
2020-11-02 17:13:40 -0500

    back to script.

--- search/build.sh
+++ search/build.sh
@@ -4,13 +4,29 @@
 curl -L https://install.meilisearch.com | sh
 
 # Install poetry
-# pip install --upgrade pip
-# pip install poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-. .poetry/bin/poetry
+curl https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py >> get-poetry.py
+python get-poetry.py --version $POETRY_VERSION
 
-# Install dependencies for scrapy
-poetry install
+. $PLATFORM_APP_DIR/.poetry/env
+echo ". $PLATFORM_APP_DIR/.poetry/env" >> ~/.bash_profile
+
+# poetry config virtualenvs.create false
+# poetry config virtualenvs.in-project true
+poetry install -vvv
 
 # Make scraping script executable
 chmod +x ./scrape.sh
+# Install Meilisearch
+# curl -L https://install.meilisearch.com | sh
+
+# # Install poetry
+# # pip install --upgrade pip
+# # pip install poetry
+# curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+# . .poetry/bin/poetry
+
+# # Install dependencies for scrapy
+# poetry install
+
+# # Make scraping script executable
+# chmod +x ./scrape.sh
