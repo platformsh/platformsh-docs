c825cf3df3c28ea98f6a77be9d25b0ef80c15243
chadcarlson <chad.carlson@platform.sh>
2020-11-05 16:48:35 -0500

    script.

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -5,10 +5,20 @@ moveDataToNS(){
       cp public/index.json public/scripts/xss/dist/config/index.json
 }
 
+buildSearchApp(){
+    echo "Building the search app"
+    # Build Interface app for XSS
+    cd $PLATFORM_APP_DIR/public/scripts/xss
+#     npm install
+    npm run-script build
+}
+
 set -e
+
+buildSearchApp
 
 # export PUBLIC_TOKEN=$(curl -H "X-Meili-API-Key: $PLATFORM_PROJECT_ENTROPY" -X GET "search.internal/keys" | jq -r ".public")
 
-moveDataToNS
+# moveDataToNS
 
-node test.js
+# node test.js
