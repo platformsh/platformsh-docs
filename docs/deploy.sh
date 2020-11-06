dfaf1461c604cb037b328e3c22dee5276b7c2592
chadcarlson <chad.carlson@platform.sh>
2020-11-06 10:48:47 -0500

    save it.

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -5,7 +5,7 @@ createSearchConfig(){
     export MEILI_INDEX=$(curl -s -H "X-Meili-API-Key: $PLATFORM_PROJECT_ENTROPY" -X GET "http://search.internal/indexes" | jq -r ".[0].uid")
     export MEILI_TOKEN=$(curl -s -H "X-Meili-API-Key: $PLATFORM_PROJECT_ENTROPY" -X GET "search.internal/keys" | jq -r ".public")
     export MEILI_URL=$(echo $PLATFORM_ROUTES | base64 --decode | jq -r 'to_entries[] | select(.value.id == "search") | .key')
-    # Delete config file in mount if it exists
+    # Delete config file in the mount if it exists
     [ ! -e public/scripts/xss/dist/config.json ] || rm public/scripts/xss/dist/config.json
     # Make the config file
     echo $( jq -n --arg ix $MEILI_INDEX --arg key $MEILI_TOKEN --arg url $MEILI_URL '{index: $ix, public_key: $key, url: $url}' ) >> public/scripts/xss/dist/config.json
