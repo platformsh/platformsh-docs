889ed803d3520128bfc5f15f75a44a32d82dca6a
chadcarlson <chad.carlson@platform.sh>
2021-06-30 10:21:06 -0400

    double check.

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -4,6 +4,7 @@
 set -e
 
 # Download Hugo
+echo "Download Hugo"
 DOWNLOAD=https://github.com/gohugoio/hugo/releases/download/v$HUGOVERSION/hugo_${HUGOVERSION}_Linux-64bit.tar.gz
 wget --quiet -c $DOWNLOAD -O - | tar -xz
 
