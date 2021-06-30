f9827687f1be3945b5d69737981bb8f671fb8640
chadcarlson <chad.carlson@platform.sh>
2021-06-30 10:57:45 -0400

    app.yaml var not available.

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -4,6 +4,7 @@
 set -e
 
 # Download Hugo
+printenv
 echo "Download Hugo - $HUGOVERSION"
 DOWNLOAD=https://github.com/gohugoio/hugo/releases/download/v$HUGOVERSION/hugo_${HUGOVERSION}_Linux-64bit.tar.gz
 # wget --quiet -c $DOWNLOAD -O - | tar -xz
