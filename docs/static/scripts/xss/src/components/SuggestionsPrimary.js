249917fd7ea6480893294268a666eaad7c371cab
Aaron Collier <aaron@collier.cz>
2022-01-07 17:44:14 +0100

    :sparkles: Allow keywords and style result matches

--- docs/static/scripts/xss/src/components/SuggestionsPrimary.js
+++ docs/static/scripts/xss/src/components/SuggestionsPrimary.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
-import React from 'react'
+import DOMPurify from 'dompurify';
+import React from 'react';
 
 // This class defines the template for primary search results,
 // which in this case are documents coming from the public documentation.
@@ -19,16 +20,24 @@ const SuggestionsPrimary = ({ hits, title }) => {
           {r.title}
         </a>
       </h5>
-      {/* Add keywords if present */}
-      {r.keywords
+      {/* Add keywords if they match */}
+      {/* eslint-disable-next-line no-underscore-dangle */}
+      {r._matchesInfo.keywords
         && (
         <p>
-          <strong>Keywords:</strong>
-          {/* Separate the keywords by commas, except the last one */}
-          {r.keywords.map((keyword, index, keywords) => `${keyword}${keywords.length - 1 > index ? ', ' : ''}`)}
+          Keywords:
+          {' '}
+          {/* eslint-disable-next-line no-underscore-dangle */}
+          {r._formatted.keywords.map((keyword, index, keywords) => {
+            /* Sanitize and separate the keywords by commas, except the last one */
+            const sanitizedKeyword = `${DOMPurify.sanitize(keyword)}${keywords.length - 1 > index ? ', ' : ''}`
+            /* eslint-disable-next-line react/no-danger */
+            return <span dangerouslySetInnerHTML={{ __html: sanitizedKeyword }} />
+          })}
         </p>
         )}
-      {r.text}
+      {/* eslint-disable-next-line no-underscore-dangle, react/no-danger */}
+      <p dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(r._formatted.text) }} />
     </li>
   ))
   return (
