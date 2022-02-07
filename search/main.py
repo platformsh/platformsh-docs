4de8e7993c3af4f0432877f4e41079d8acd756c0
Aaron Collier <aaron@collier.cz>
2022-02-07 18:14:49 +0100

    :bug: Prioritize keywords more

--- search/main.py
+++ search/main.py
@@ -21,9 +21,9 @@ class Search:
         # Below are Platform.sh custom settings for how the search engine functions.
 
         # Data available to the dropdown React app in docs, used to fill out autocomplete results.
-        self.displayed_attributes = ['title', 'text', 'url', 'keywords', 'site', 'section']
+        self.displayed_attributes = ['keywords', 'title', 'text', 'url', 'site', 'section']
         # Data actually searchable by our queries.
-        self.searchable_attributes = ['title', 'pageUrl', 'keywords', 'section', 'text', 'url']
+        self.searchable_attributes = ['keywords', 'title', 'pageUrl', 'section', 'text', 'url']
 
         # Show results for one query with the listed pages, when they by default would not show up as best results.
         # Note: these aren't automatically two-way, which is why they're all defined twice.
