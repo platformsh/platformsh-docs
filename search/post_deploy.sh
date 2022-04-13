b8caad4c2ac3da4dcda073b6bd8efc587239e332
Aaron Collier <aaron@collier.cz>
2022-04-13 15:06:12 +0200

    :arrow_up: Update Scrapy

--- search/post_deploy.sh
+++ search/post_deploy.sh
@@ -29,7 +29,7 @@ scrape(){
         index_spider=$(jq --arg index "$index" '.indexes[$index].spider' $DATA)
         spider=`echo $index_spider | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/'`
         echo "- Scraping $index..."
-        poetry run scrapy runspider --output -t=jsonlines -o output/$index.json $spider -L ERROR
+        poetry run scrapy runspider -o output/$index.json $spider -L ERROR
     done
 }
 
