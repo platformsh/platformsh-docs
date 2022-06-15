803791d2ba7f7be42f2da1e965728471aab29cf0
Aaron Collier <aaron@collier.cz>
2022-06-15 18:41:06 +0200

    ✨ Record feedback in database

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,44 @@
 const express = require('express')
 const path = require('path')
+const mysql = require("mysql2");
+const config = require("platformsh-config").config();
 
 const app = express()
 
 app.use(express.json())
 
 /* Listen for feedback posts */
-app.post("/feedback", (req,res) => {
+app.post("/feedback/submit", (req, res) => {
+  const credentials = config.credentials("database");
 
-  console.log(req.body)
+  const connection = mysql.createConnection({
+    host: credentials.host,
+    port: credentials.port,
+    user: credentials.username,
+    password: credentials.password,
+    database: credentials.path,
+  });
+
+  const feedback = req.body
+
+  // Create a feedback table if it doesn't exist
+  connection.query(
+    `CREATE TABLE IF NOT EXISTS Feedback (
+      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      date DATE NOT NULL,
+      url VARCHAR(100) NOT NULL,
+      feedback VARCHAR(10) NOT NULL
+    )`,
+    (err) => res.status(500).send("Error creating a table for feedback")
+  );
+
+  // Insert feedback record
+  connection.query(
+    `INSERT INTO Feedback (date, url, feedback)
+    VALUES
+      ('${feedback.date}', '${feedback.url}', '${feedback.feedback}');`,
+      (err) => res.status(500).send("Error entering feedback into database")
+  );
 
   res.status(200).send("Feedback recorded");
 
@@ -21,6 +51,6 @@ app.get('/*', (req, res) => {
   })
 })
 
-app.listen(8888, () => {
-  console.info(`ExpressJS listening on 8888.`)
+app.listen(config.port, () => {
+  console.info(`ExpressJS listening on ${config.port}.`)
 })
