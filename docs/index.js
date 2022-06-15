b6accf62f6766223510257b81bfae20a45bdfbc4
Aaron Collier <aaron@collier.cz>
2022-06-15 20:44:56 +0200

    ✨ Add basic report

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const config = require("platformsh-config").config();
 
 const app = express()
@@ -8,10 +8,10 @@ const app = express()
 app.use(express.json())
 
 /* Listen for feedback posts */
-app.post("/feedback/submit", (req, res) => {
+app.post("/feedback/submit", async (req, res) => {
   const credentials = config.credentials("database");
 
-  const connection = mysql.createConnection({
+  const connection = await mysql.createConnection({
     host: credentials.host,
     port: credentials.port,
     user: credentials.username,
@@ -22,26 +22,84 @@ app.post("/feedback/submit", (req, res) => {
   const feedback = req.body
 
   // Create a feedback table if it doesn't exist
-  connection.query(
-    `CREATE TABLE IF NOT EXISTS Feedback (
-      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      date DATE NOT NULL,
-      url VARCHAR(100) NOT NULL,
-      feedback VARCHAR(10) NOT NULL
-    )`,
-    (err) => res.status(500).send("Error creating a table for feedback")
-  );
+  try {
+    await connection.query(
+      `CREATE TABLE IF NOT EXISTS Feedback (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        date DATETIME NOT NULL,
+        url VARCHAR(100) NOT NULL,
+        feedback VARCHAR(10) NOT NULL
+      )`
+    );
+  } catch (err) { res.status(500).send("Error creating a table for feedback") }
 
   // Insert feedback record
-  connection.query(
-    `INSERT INTO Feedback (date, url, feedback)
-    VALUES
-      ('${feedback.date}', '${feedback.url}', '${feedback.feedback}');`,
-      (err) => res.status(500).send("Error entering feedback into database")
-  );
+  try {
+    await connection.query(
+      `INSERT INTO Feedback (date, url, feedback)
+      VALUES
+        ('${feedback.date}', '${feedback.url}', '${feedback.feedback}');`
+    );
+  } catch (err) { res.status(500).send("Error entering feedback into database") }
 
   res.status(200).send("Feedback recorded");
 
+})
+
+/* Return a simple table of feedback submissions */
+app.get('/feedback/report.html', async (req, res) => {
+  const credentials = config.credentials("database");
+
+  const connection = await mysql.createConnection({
+    host: credentials.host,
+    port: credentials.port,
+    user: credentials.username,
+    password: credentials.password,
+    database: credentials.path,
+  });
+
+  // Get all feedback submissions
+  const [rows] = await connection.query("SELECT * FROM Feedback");
+  
+  const outputRows = rows
+    .map(({ date, url, feedback }) => `<tr><td>${new Date(date).toDateString()}</td><td>${url}</td><td>${feedback}</td></tr>\n`)
+    .join("\n");
+
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>Docs feedback</title>
+        <style>
+          table {
+            border-collapse: collapse;
+          }
+          
+          th, td {
+            border: 1px solid black;
+            padding: 0.25rem 0.5rem;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Feedback on specific docs pages</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>URL</th>
+              <th>Feedback</th>
+            </tr>
+          </thhead>
+          <tbody>
+            ${outputRows}
+          </tbody>
+        </table>
+      </body>
+    </html>
+    `;
+
+  res.send(html)
 })
 
 /* Handle 404 requests */
