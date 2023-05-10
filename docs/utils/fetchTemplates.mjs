bf87ac98afe4e2dfbf0f22d53d0e77b40a29be29
Paul Gilzow <paul.gilzow@platform.sh>
2023-05-10 14:10:06 -0500

    refactors code to use imported GH API related items from common.mjs

--- docs/utils/fetchTemplates.mjs
+++ docs/utils/fetchTemplates.mjs
@@ -5,42 +5,30 @@ import pLimit from 'p-limit';
 import * as path from "path";
 import { existsSync, copyFile, copyFileSync } from 'node:fs';
 import { Octokit } from "octokit";
-
+import * as ghCommon from './common.mjs'
 
 // Limit the number of requests running at once to not run out of memory or anything
 const fetchConcurrency = pLimit(10);
-
-const githubOrgName = "platformsh"
-const templateBuildRepoName = "template-builder"
-const pathToTemplatesInTB = "templates"
-
-const ghHeaders = {
-  'X-GitHub-Api-Version': '2022-11-28'
-}
 
 // where our cache for the template file is located
 const cachedTemplatePath = process.env.PLATFORM_CACHE_DIR
 // the name of the cached template file
 const cachedTemplateName = "templates.yaml"
 // full path + name
-const cachedTemplateFileFull = cachedTemplatePath + path.sep + cachedTemplateName
+const cachedTemplateFileFull = path.join(cachedTemplatePath,cachedTemplateName)
 // where the template file should ultimately end up before deployment
-const destinationTemplatePath = process.cwd() + path.sep + "data"
+const destinationTemplatePath = path.join(process.cwd(),"data")
 // full path + name
-const destinationTemplateFull = destinationTemplatePath + path.sep + cachedTemplateName
-
-const ocktokit = new Octokit ({
-  auth: process.env.GITHUB_API_TOKEN
-})
+const destinationTemplateFull = path.join(destinationTemplatePath,cachedTemplateName)
 
 
 // Get all templates from the template builder repo directory structure
 const getTemplateList = async () => {
-  const response = await ocktokit.request("GET /repos/{owner}/{repo}/contents/{path}",{
-    owner: githubOrgName,
-    repo: templateBuildRepoName,
-    path: pathToTemplatesInTB,
-    headers: ghHeaders
+  const response = await ghCommon.ocktokit.request("GET /repos/{owner}/{repo}/contents/{path}",{
+    owner: ghCommon.githubOrgName,
+    repo: ghCommon.templateBuildRepoName,
+    path: ghCommon.pathToTemplatesInTB,
+    headers: ghCommon.ghHeaders,
   }).catch(err => console.error("The request to get the templates failed: ", err));
   //const response = await axios.get("https://api.github.com/repos/platformsh/template-builder/contents/templates")
     //.catch(err => console.error("The request to get the templates failed: ", err))
@@ -55,20 +43,18 @@ const YAML_SCHEMA = yaml.DEFAULT_SCHEMA.extend([IncludeYamlType]);
 
 // Look for the runtime in the app configuration file in the template builder repo
 const getTemplateRuntime = async (name, url, count) => {
-  let configPath = `${pathToTemplatesInTB}/${name}/${url}.platform.app.yaml`
+  let configPath = `${ghCommon.pathToTemplatesInTB}/${name}/${url}.platform.app.yaml`
 
   try {
-    const templateConfig = await ocktokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
-      owner: githubOrgName,
-      repo: templateBuildRepoName,
+    const templateConfig = await ghCommon.ocktokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+      owner: ghCommon.githubOrgName,
+      repo: ghCommon.templateBuildRepoName,
       path: configPath
     })
 
     let runtimeData = ""
 
-    // @todo we should probably wrap the attempt to decode in a try catch as well
-    let Butter = new Buffer.from(templateConfig.data.content, 'base64')
-    let templateConfigDecoded = Butter.toString('utf-8')
+    let templateConfigDecoded = ghCommon.decodeBase64Contents(templateConfig.data.content)
     //console.log(`Decoded contents for template ${name}:`)
     //console.log(templateConfigDecoded)
 
@@ -111,23 +97,21 @@ const getTemplateRuntime = async (name, url, count) => {
 
 // Get info on each template based on the information file inside the templace builder repo
 const getTemplatesFullInfo = async (templateInfo) => {
-  let continueTemplateCall = await reportAPILimit()
-
-  // for now we're going to make sure we have api calls remaining before making another call
-  if(!continueTemplateCall) {
-    return {shortname: name, runtime: "over-api-limits" }
+  // if we're over the limit, it'll just error out so no use continuing
+  if(await ghCommon.reportOverAPILimit()) {
+    // @todo what should we do here besides the console error that we're over the limit?
+    return {shortname: templateInfo.name, runtime: "over-api-limits" }
   }
 
-  const templateData = await ocktokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
-    owner: githubOrgName,
-    repo: templateBuildRepoName,
-    path: `${pathToTemplatesInTB}/${templateInfo.name}/.platform.template.yaml`,
-    headers: ghHeaders
+  const templateData = await ghCommon.ocktokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+    owner: ghCommon.githubOrgName,
+    repo: ghCommon.templateBuildRepoName,
+    path: `${ghCommon.pathToTemplatesInTB}/${templateInfo.name}/.platform.template.yaml`,
+    headers: ghCommon.ghHeaders,
   })
 
   if(200 === templateData.status) {
-    let Butter = new Buffer.from(templateData.data.content,'base64')
-    let templateContentDecoded = Butter.toString('utf-8')
+    let templateContentDecoded = ghCommon.decodeBase64Contents(templateData.data.content)
 
     let info = {}
     try {
@@ -154,44 +138,6 @@ const getTemplatesFullInfo = async (templateInfo) => {
   }
 }
 
-
-  // console.log(`Did I get something back for ${templateInfo.name}?`)
-  // console.log(templateResponse.data.content)
-
-  //   .then(templateResponse => {
-  //   console.log(`Did I get something back for ${templateInfo.name}?`)
-  //   console.log(templateResponse.data.content)
-  //   let buff = new Buffer.from(templateResponse.data.content,'base64')
-  //   let templateContents = buff.toString('utf-8');
-  //   return templateContents
-  // }).then(async templateResponse => {
-  //     let info = {}
-  //     try {
-  //       info = yaml.load(templateResponse).info
-  //     } catch {
-  //       console.error(`Error loading the YAML file with info on the ${templateInfo.name} template`)
-  //       return
-  //     }
-  //
-  //     // Get runtime data from a different file
-  //     const runtime = await getTemplateRuntime(templateInfo.name, `${templateInfo.url}/files/`, 1);
-  //
-  //     return {
-  //       shortname: templateInfo.name,
-  //       name: info.name,
-  //       repo: `https://github.com/platformsh-templates/${templateInfo.name}`,
-  //       description: info.description,
-  //       image: info.image,
-  //       deploy: `https://console.platform.sh/projects/create-project?template=https://raw.githubusercontent.com/platformsh/template-builder/master/templates/${templateInfo.name}/.platform.template.yaml`,
-  //       content: info.notes[0].content,
-  //       runtime: runtime
-  //     };
-  //   })
-  //   .catch(err => console.error(`Error fetching template URL for the ${templateInfo.name} template: ${err}`))
-  //
-  //   return templateData
-//}
-
 // Get a list of templates from GitHub and return a YAML string with info on all
 const fetchTemplates = async () => {
   const templates = await getTemplateList()
@@ -202,9 +148,8 @@ const fetchTemplates = async () => {
     .map(template => {
       // @todo we should check current api limits and skip if we've exceeded
       const name = template.name
-      // @todo url can be removed as we're no longer using it
-      const url = `https://raw.githubusercontent.com/platformsh/template-builder/master/templates/${name}`
-      return fetchConcurrency(getTemplatesFullInfo, { name, url })
+
+      return fetchConcurrency(getTemplatesFullInfo, template)
     })
 
   const allInfo = await Promise.all(templateList)
@@ -233,10 +178,12 @@ const writeTemplateInfo = async () => {
 
 // copies the cached version of the templates file from build cache to where hugo expects it
 const getTemplateInfo = async () => {
-  await reportAPILimit()
   try {
     // if the file doesn't exist. we need to create it and copy it into the build cache
     if (!existsSync(cachedTemplateFileFull)) {
+      if (await ghCommon.reportOverAPILimit()) {
+        console.error(`We need to cache the ${cachedTemplateName} file but we can't because we're over the GH API limit!`)
+      }
       await writeTemplateInfo()
     } else {
       console.log(cachedTemplateName + " already exists.")
@@ -253,17 +200,6 @@ const getTemplateInfo = async () => {
     console.log(`Errors were encountered attempting to copy the cached ${cachedTemplateName} file`)
     console.log(`Attempted to copy from ${cachedTemplateFileFull} to ${destinationTemplateFull}`)
   }
-  await reportAPILimit()
-}
-
-const reportAPILimit = async () => {
-  const apiLimit = await ocktokit.request('GET /rate_limit', {
-    headers: ghHeaders
-  })
-
-  console.log(`Number of requests remaining: ${apiLimit.data.rate.remaining} out of ${apiLimit.data.rate.limit}`)
-
-  return (apiLimit.data.rate.remaining > 0)
 }
 
 console.log(`
