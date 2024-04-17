2c8f1094d7b29c85d641b95e10b72eac7050bf8f
chadcarlson <chad.carlson@platform.sh>
2024-04-17 14:38:48 -0400

    Fix meilisearch grabbing wrong description.

--- search/main.py
+++ search/main.py
@@ -26,9 +26,9 @@ class Search:
         # Below are Platform.sh custom settings for how the search engine functions.
 
         # Data available to the dropdown React app in docs, used to fill out autocomplete results.
-        self.displayed_attributes = ['keywords', 'title', 'text', 'url', 'site', 'section']
+        self.displayed_attributes = ['keywords', 'title', 'description', 'url', 'site', 'section']
         # Data actually searchable by our queries.
-        self.searchable_attributes = ['keywords', 'title', 'pageUrl', 'section', 'text', 'url']
+        self.searchable_attributes = ['keywords', 'title', 'pageUrl', 'section', 'description', 'url']
 
         # Show results for one query with the listed pages, when they by default would not show up as best results.
         # Note: these aren't automatically two-way, which is why they're all defined twice.
