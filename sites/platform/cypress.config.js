ec6b715b501711958f2fe8d6a144f5e13aa8cce6
Paul Gilzow <paul.gilzow@platform.sh>
2024-06-14 15:34:17 -0500

    updates config files and test file, adds github actions file

--- sites/platform/cypress.config.js
+++ sites/platform/cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:1313/',
+    env: {
+      barfoo: "from config",
+      env: "local",
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- sites/platform/cypress/e2e/search.cy.js
+++ sites/platform/cypress/e2e/search.cy.js
@@ -1,14 +1,40 @@
+//Both should return results for "opensearch"
+//Only Upsun should return a match for "vertical scaling"
+//Only platform should return a match for "24.55 gb"
 describe("Home",()=>{
   beforeEach(()=>{
-    cy.visit("http://localhost:1313/")
+    if('local' == Cypress.env('environment')) {
+      cy.intercept("/indexes/platform_docs/search*", { "hits":[] })
+    }
+
+    cy.visit("/")
   })
 
   context("Search tests",()=>{
-    specify("Search feature works", () => {
+    it("Search feature works", () => {
+      ///cy.wait("@noSearchResults")
+      //cy.intercept("/\/indexes\/platform_docs\/search[^q]+q=opensearch/",{ fixture: "searchosresults" }).as("searchresultsopensearch")
+      console.log("Env is " + Cypress.env('environment'))
+      console.log("Barfoo is " + Cypress.env('barfoo'))
+      if('local' == Cypress.env('environment')) {
+        cy.intercept({
+          pathname: '/indexes/platform_docs/search',
+          query: {
+            q: 'opensearch'
+          }
+        },{ fixture: "searchosresults" }).as("searchresultsopensearch")
+      }
+
       cy.get("#searchwicon-header").type("opensearch")
+
+      if ('local' == Cypress.env('environment')) {
+        cy.wait('@searchresultsopensearch')
+      }
+
       cy.get("#xssroot").find("h2")
         .should("exist")
         .contains("Documentation")
+      cy.get("#xssroot").find("li").contains("OpenSearch").should("exist")
     })
   })
 })
