170dc6aa1a7b6b0c126857943db9498188954419
Tyler Mills <tyler.mills@platform.sh>
2024-09-03 17:32:32 -0400

    Use postcss for minification

--- themes/psh-docs/postcss.config.js
+++ themes/psh-docs/postcss.config.js
@@ -7,5 +7,15 @@ module.exports = {
     require('autoprefixer')({
       path: [themeDir]
     }),
+    ...process.env.HUGO_ENVIRONMENT === 'production'
+      ? [
+        require('cssnano')({
+          preset: ['default', {
+            discardComments: { removeAll: true }, // Ensure all comments are removed
+            normalizeWhitespace: true, // Ensures that whitespace is minimized
+          }]
+        })
+      ]
+      : []
   ]
 }
